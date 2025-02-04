3dd9b10a439ad5b88b2702fa005098671af408d4
Peter Todorov <peter.todorov@live.com>
2025-02-04 09:28:30 +0200

    Enable MacBook metal for faster processing

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from bottle import route, run, request, response, static_file
 from llama_cpp import Llama
 import time
 
-llm = Llama(model_path="mistral-7b-v0.1.Q4_K_M.gguf")
+# Enable MAcBook M2 metal with n_gpu_layers=-1
+llm = Llama(model_path="mistral-7b-v0.1.Q4_K_M.gguf", n_gpu_layers=-1)
 
 @route('/')
 def index():
@@ -11,9 +12,9 @@ def index():
 @route('/chat', method='POST')
 def chat():
     message = request.json.get('message', '')
-    #output = llm(message, max_tokens=2048, temperature=0.7)
-    time.sleep(3)  # Add 3 second delay
+    output = llm(message, max_tokens=2048, temperature=0.7)
+    #time.sleep(3)  # Add 3 second delay
     response.content_type = 'application/json'
-    return {'response': "Hello, how are you?"} #output['choices'][0]['text']}
+    return {'response': output['choices'][0]['text']}
 
 run(host='localhost', port=8080)
